11ee168fc2203efb8da6a7bcddf4757811cbde5b
Efrain Perez <efrain.perez@stemconnector.com>
2017-10-29 22:40:09 -0400

    Changing between header tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  private headerName: string = 'recipes';
+
+
+  public setHeaderName(headerName: string) {
+    this.headerName = headerName;
+  }
+
+  public getHeaderName(): string {
+    return this.headerName;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  private headerName = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
+
+  public setHeaderName(headerName: string) {
+    this.headerName.emit(headerName);
+  }
+
+  @Output('headerSection')
+  public get name() {
+    return this.headerName;
+  }
+
+
 
 }
